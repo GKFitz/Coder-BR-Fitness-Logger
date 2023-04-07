644b9d89bea858ca3d56f13bcda64e36f29ec171
GKFitz123 <gkf2103@gmail.com>
2023-04-07 14:39:50 -0400

    index and show route added

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -2,6 +2,42 @@ const express = require('express');
 const Workout = require('../models/workout');
 const router = express.Router();
 
+//I
+router.get("/", async(req, res) => {
+    const allWorkouts = await Workout.find({})
+    res.render("workouts/index.ejs", {
+        workout: allWorkouts
+    })
+})
+
+
+//N
+
+
+
+//D
+
+
+
+//U
+
+
+
+//C
+
+
+
+//E
+
+
+
+//S
+router.get("/:id", async(req, res) => {
+    const foundWorkout = await Workout.findById(req.params.id).exec()
+    res.render("workouts/show.ejs", {
+        workout: foundWorkout
+    })
+})
 
 
 module.exports = router;
