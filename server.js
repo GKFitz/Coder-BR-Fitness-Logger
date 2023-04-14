efbf0af5e9a2992c121cc751ac701d6a804dc19f
GKFitz123 <gkf2103@gmail.com>
2023-04-14 04:45:41 -0400

    boostrap carousel installed

--- server.js
+++ server.js
@@ -27,12 +27,16 @@ db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 //MiddleWares
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 //use public folder for static assets
+// app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static('public'))
+// app.use('/images', express.static('images'));
+
 
 app.use(
     session({
